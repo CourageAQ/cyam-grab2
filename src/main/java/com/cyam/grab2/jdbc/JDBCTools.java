c54f4849d949918449005da031e3ca6146d2b5a9
BL-Y <BL-Y@DESKTOP-7ALGDBP>
2017-07-12 11:42:46 +0800

    数据库地址改变

--- src/main/java/com/cyam/grab2/jdbc/JDBCTools.java
+++ src/main/java/com/cyam/grab2/jdbc/JDBCTools.java
@@ -9,7 +9,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class JDBCTools {
-	private static final String url = "jdbc:jtds:sqlserver://10.150.127.184:1433/cyam";
+	private static final String url = "jdbc:jtds:sqlserver://win-sql:1433/cyam";
 //	private static final String url = "jdbc:jtds:sqlserver://localhost:1433/cyam";
 	private static final String username = "yqrl";
 //	private static final String username = "sa";
